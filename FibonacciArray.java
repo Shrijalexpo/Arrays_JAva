85b628ba0d66e608789e22853245a03864e48c85
Vedant Kesarwani <78657126+GeniusVedant@users.noreply.github.com>
2022-07-01 18:53:41 +0530

    Add files via upload

---
+++ FibonacciArray.java
@@ -0,0 +1,33 @@
+// Q9
+class FibonacciArray{
+    int[] fibonacci = new int[20], difference = new int[19];
+    void storeFibonacci(){
+        int num1 = 1, num2 = 1;
+        fibonacci[0] = 1;
+        for(int counter = 1; counter < 20; counter ++){
+            fibonacci[counter] = num2;
+            int temp = num1;
+            num1 = num2;
+            num2 = num1 + temp;
+        }
+    }
+    void findDifference(){
+        for(int counter = 1; counter < 20; counter ++){
+            difference[counter - 1] = fibonacci[counter] - fibonacci[counter - 1];
+        }
+    }
+    void display(){
+        int counter;
+        System.out.print("{");
+        for(counter = 0; counter < 18; counter ++){
+            System.out.print(difference[counter] + ", ");
+        }
+        System.out.print(difference[counter] + "}");
+    }
+    public static void main(String[] Args){
+        FibonacciArray ob = new FibonacciArray();
+        ob.storeFibonacci();
+        ob.findDifference();
+        ob.display();
+    }
+}
